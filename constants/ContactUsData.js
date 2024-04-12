186de47d76b1e3bf9535815b248e335ef6e30159
jordanaverps <142835883+jordanaverps@users.noreply.github.com>
2024-04-13 01:14:11 +0530

    update icons formate .webp into png

--- constants/ContactUsData.js
+++ constants/ContactUsData.js
@@ -2,32 +2,32 @@ export const ContactUsData = [
   {
     id: 1,
     text: "2 Jalan Kilang Barat, 01-01 Singapore 159346",
-    icon: "/images/contact-1.webp",
+    icon: "/images/contact-1.png",
   },
   {
     id: 2,
     text: "Shop New & Pre-Loved Cars",
-    icon: "/images/contact-2.webp",
+    icon: "/images/contact-2.png",
   },
   {
     id: 3,
     text: "+65 6266 5555",
-    icon: "/images/contact-3.webp",
+    icon: "/images/contact-3.png",
   },
   {
     id: 4,
     text: "Opening Hours",
     helper: "Mon to Sun: 10am to 6.30pm",
-    icon: "/images/contact-4.webp",
+    icon: "/images/contact-4.png",
   },
   {
     id: 5,
     text: "enquiries@supremecars.com.sg",
-    icon: "/images/contact-5.webp",
+    icon: "/images/contact-5.png",
   },
   {
     id: 6,
     text: "Arrange a Workshop",
-    icon: "/images/contact-6.webp",
+    icon: "/images/contact-6.png",
   },
 ];
